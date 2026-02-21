1e143148ad22543f52075c31af3c7a3ede8c0bb4
shridharathi <shridharathinarayanan@gmail.com>
2026-02-20 22:18:49 -0600

    oops this is correct

--- src/assets/experience.js
+++ src/assets/experience.js
@@ -1,6 +1,5 @@
 const EXPERIENCE = [
   {
-<<<<<<< HEAD
     name: 'Replicate (acq. Cloudflare)',
     year: '2025 —',
     description: 'Shipping the latest AI models.',
@@ -11,27 +10,6 @@ const EXPERIENCE = [
     name: 'FLORA AI',
     year: '2024',
     description: 'Shipped AI tools for creatives.',
-=======
-    name: 'Replicate',
-    dates: {
-      start: '03.2025',
-    },
-    description:
-      'Shipping the latest AI models and tools to the community.',
-       
-    position: 'AI Engineer',
-    link: "https://www.replicate.com",
-  },
-  {
-    name: 'FLORA AI',
-    dates: {
-      start: '08.2024',
-      end: '12.2024',
-    },
-    description:
-      'AI tools for creatives.',
-       
->>>>>>> 224795c336630e069cd593cac0652628a297bc21
     position: 'AI Product Engineer',
     link: 'https://www.florafauna.ai/',
   },
@@ -67,19 +45,5 @@ const EXPERIENCE = [
     position: 'Data Science Intern',
     link: 'https://agu.confex.com/agu/fm20/meetingapp.cgi/Paper/735906',
   },
-<<<<<<< HEAD
-=======
-  {
-    name: 'Computational Soft Matter Lab, UoM',
-    dates: {
-      start: '07.2017',
-      end: '12.2018',
-    },
-    description:
-      'Implemented Metropolis-Monte Carlo polymer folding scheme using spring potential energy function – folded “hras” protein 86% to its correct structure. Presented @ Intel ISEF 2018.',
-    position: 'Research Assistant',
-    link: 'https://www.memphis.edu/physics/faculty-laradji/index.php',
-  }
->>>>>>> 224795c336630e069cd593cac0652628a297bc21
 ];
 export default EXPERIENCE;
